32256d6908ca9a511b346c9abfb33be7eee2462b
m0rg123 <morgana.pasquini@studenti.unitn.it>
2023-12-22 17:32:41 +0100

    Aggiungi annuncio (venditore placeholder)

---
+++ src/app/annunci/new/page.tsx
@@ -0,0 +1,95 @@
+'use client'
+import { RequiredField } from "@/app/helpers/validation";
+import {Button, Form, message, Select} from "antd"
+import axios from "axios";
+import Link from "next/link"
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+
+interface annuncioType {
+    title: string;
+    author: string;
+    category: string;
+    ISBN: string;
+    price: number;
+    condition: string;
+    seller: string;
+}
+function AddAnnuncio () {
+    const router = useRouter();
+    const onAddAnnuncio = async (values: annuncioType) => {
+        console.log(values);
+        try {
+            await axios.post("/api/annunci/new", values);
+            message.success("Successfully added post");
+            //router.push("/auth/login");//visualizza annuncio appena creato
+        } catch (error: any) {
+            message.error(error.response.data.message);
+        }
+    };
+
+    return (
+        <div>
+            <div className="">
+                <Form className='w-[500px]' layout='vertical' onFinish={onAddAnnuncio}>
+                    <h1 className='text-2x1 font-bold'>Add Book</h1>
+                    <hr />
+                    <br />
+
+                    <Form.Item name="title" label="Title"
+                               rules={RequiredField('Please insert the title')}
+                               initialValue={''}>
+                        <input type='text' />
+                    </Form.Item>
+                    <Form.Item name="author" label="Author"
+                               rules={RequiredField('Please insert the author')}
+                               initialValue={''}>
+                        <input type='text' />
+                    </Form.Item>
+                    <Form.Item name="category" label="Category"
+                               rules={RequiredField('Please choose the category')}
+                               initialValue={''}>
+                        <select>
+                            <option value="university">University</option>
+                            <option value="action">Action</option>
+                            <option value="horror">Horror</option>
+                            <option value="non-fiction">Non-Fiction</option>
+                            <option value="comic">Comic</option>
+                        </select>
+                    </Form.Item>
+                    <Form.Item name="ISBN" label="ISBN"
+                               rules={RequiredField('Please insert the ISBN')}
+                               initialValue={''}>
+                        <input type='text' />
+                    </Form.Item>
+                    <Form.Item name="price" label="Price"
+                               rules={RequiredField('Please insert the price')}
+                               initialValue={''}>
+                        <input type='number' />
+                    </Form.Item>
+                    <Form.Item name="condition" label="Condition"
+                               rules={RequiredField('Please choose the condition')}
+                               initialValue={''}>
+                        <select>
+                            <option value="new">New</option>
+                            <option value="good">Good</option>
+                            <option value="used">Used</option>
+                            <option value="damaged">Damaged</option>
+                        </select>
+                    </Form.Item>
+                    <Form.Item name="seller" label="Seller"
+                               rules={RequiredField('placeholder seller')}
+                               initialValue={'placeholder'}>
+                        <input type='text' />
+                    </Form.Item>
+
+                    <Button type='primary' htmlType='submit' block>
+                        Post the ad
+                    </Button>
+                </Form>
+            </div>
+        </div>
+
+    )
+}
+export default AddAnnuncio
---
+++ src/app/api/annunci/new/route.ts
@@ -0,0 +1,29 @@
+import { connectDB } from "../../../../../dbConfig";
+import Ad from "@/app/models/announceModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+    try {
+
+        const reqBody = await request.json();
+        const newAd = new Ad(reqBody);
+
+        await newAd.save();
+
+        return NextResponse.json({
+            message: "Ad created successfully",
+            data: newAd,
+        })
+    } catch (error: any) {
+        return NextResponse.json({
+                message: error.message,
+            },
+            {
+                status: 400
+            }
+        );
+    }
+}
