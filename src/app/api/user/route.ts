96b68ea19d154dbe2fe9b1381c1c31a1aa974a7f
A <alessandro.cornella@studenti.unitn.it>
2023-12-23 11:49:20 +0100

    added the GetUser API

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,26 @@
+import { connectDB } from "../../../../dbConfig";
+import { validateJWT } from "@/app/helpers/validatejwt";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/app/models/userModel";
+
+connectDB();
+
+export async function GET(request: NextRequest) {
+    try {
+        const userId = await validateJWT(request);
+        // retrieve the user without the password
+        const user = await User.findById(userId).select("-password");
+        return NextResponse.json({
+            data: user,
+        });
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                message: "Unauthorized",//error.message,
+            },
+            {
+                status: 401,
+            }
+        );
+    }
+}
