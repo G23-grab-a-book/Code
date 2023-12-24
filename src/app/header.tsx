8a7c3d99e179e9946fc17530de734771fff258ac
A <alessandro.cornella@studenti.unitn.it>
2023-12-24 14:11:08 +0100

    create a first header version for the front-end

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,10 +1,34 @@
-import { Button } from 'antd';
+import React from 'react';
+import { Input, Button } from 'antd';
+import { UserOutlined, WarningFilled } from '@ant-design/icons';
 
-export default function Header() {
-    return (
-      <div>
-        <h1>Grab a Book</h1>
-        <Button type='primary'>Button</Button>
-      </div>
-    );
+import './header.css'; // File CSS per lo stile dell'header
+
+const Header = () => {
+  function handleProfileClick(): void {
+    throw new Error('Function not implemented.');
   }
+
+  return (
+    <><header className="app-header">
+      <div className="left-section">
+        <div className="title">
+          <h1>Grab a Book</h1>
+        </div>
+      </div>
+      <div className="center-section">
+        <Input placeholder="Cerca libri" className="search-input" />
+      </div>
+      <div className="right-section">
+        <div className="profile">
+          <Button type="primary" shape="circle" icon={<UserOutlined />}></Button>
+        </div>
+      </div>
+      <Button type="primary" shape="round" icon={<WarningFilled />}></Button>
+    </header><div>
+        <Button type="primary" shape="circle" icon={<UserOutlined />}></Button>
+      </div></>
+  );
+};
+
+export default Header;
