7ea02b46fb5745f0c9ee7799e203f3b6aad9bc01
m0rg123 <morgana.pasquini@studenti.unitn.it>
2023-12-26 20:24:32 +0100

    Updated header with functioning search

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,33 +1,57 @@
+'use client'
 import React from 'react';
-import { Input, Button } from 'antd';
+import {Input, Button, message, Form} from 'antd';
 import { UserOutlined, WarningFilled } from '@ant-design/icons';
 
-import './header.css'; // File CSS per lo stile dell'header
+
+import './header.css';// File CSS per lo stile dell'header
+import {useRouter} from "next/navigation";
+import Link from "next/link";
 
 const Header = () => {
-  function handleProfileClick(): void {
-    throw new Error('Function not implemented.');
+  const router = useRouter();
+  function handleProfileClick(){
+      try {
+          router.push("/profile");
+      }catch (e){
+          message.error(e as any);
+      }
   }
+  const onSearch = async (value: string) => {
+      try {
+          if (value === null || value === undefined || value === "") {
+              router.push("/");
+              return;
+          }
+          router.push(`/annunci/search/?search=${value}`)
+      } catch (error) {
+          message.error((error as any).response.data.message);
+      }
+  };
 
   return (
     <><header className="app-header">
       <div className="left-section">
         <div className="title">
-          <h1>Grab a Book</h1>
+            <Link href={"/"}><h1 className="grab-a-book">Grab a Book</h1></Link>
         </div>
       </div>
       <div className="center-section">
-        <Input placeholder="Cerca libri" className="search-input" />
+          <Input.Search
+              className={"search-input"}
+              placeholder="Cerca un libro"
+              allowClear
+              enterButton="Search"
+              size="large"
+              onSearch={onSearch} />
       </div>
       <div className="right-section">
         <div className="profile">
-          <Button type="primary" shape="circle" icon={<UserOutlined />}></Button>
+          <Button type="primary" shape="circle" icon={<UserOutlined/>} onClick={handleProfileClick}></Button>
         </div>
       </div>
-      <Button type="primary" shape="round" icon={<WarningFilled />}></Button>
-    </header><div>
-        <Button type="primary" shape="circle" icon={<UserOutlined />}></Button>
-      </div></>
+
+    </header></>
   );
 };
 
