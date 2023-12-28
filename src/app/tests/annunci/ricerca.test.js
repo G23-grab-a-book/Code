acaf18f94a4f185bcfac9e639b5831d2984e514a
m0rg123 <morgana.pasquini@studenti.unitn.it>
2023-12-28 18:40:07 +0100

    Test ricerca

--- src/app/tests/annunci/ricerca.test.js
+++ src/app/tests/annunci/ricerca.test.js
@@ -1,1 +1,19 @@
-const url = "http://localhost:3000/api/annunci/";
+const url = "http://localhost:3000/api/annunci/ricerca?search=";
+const validParams = "autore:\"test_author\"";
+const emptyParams = "";
+
+describe("GET /api/annunci/ricerca", () => {
+    test("GET with valid search parameters", async () => {
+        const res = await fetch(url+validParams, {
+            method: 'GET'
+        })
+        expect((await res.json()).data.length).toBeGreaterThanOrEqual(0);
+    });
+
+    test("GET with empty search parameters", async () => {
+        const res = await fetch(url+emptyParams, {
+            method: 'GET'
+        })
+        expect((await res.json()).data.length).toEqual(0);
+    });
+});
