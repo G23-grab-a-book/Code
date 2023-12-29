d722ff8ab856a1dca2c95d7e3f68d159f2b56f72
A <alessandro.cornella@studenti.unitn.it>
2023-12-29 13:08:53 +0100

    another vercel fix

--- src/app/api_docs/page.tsx
+++ src/app/api_docs/page.tsx
@@ -4,10 +4,23 @@ import SwaggerUI from 'swagger-ui-react';
 import "swagger-ui-react/swagger-ui.css"
 import swaggerFile from './api_spec.json';
 
-export default function SwaggerPage(){
-  return (
-    <div>
-      <SwaggerUI spec={swaggerFile} />
-    </div>
-  );
+// export default function SwaggerPage(){
+//   return (
+//     <div>
+//       <SwaggerUI spec={swaggerFile} />
+//     </div>
+//   );
+// };
+
+import dynamic from "next/dynamic";
+
+const DynamicSwaggerUI = dynamic(() => import("swagger-ui-react"), {
+  ssr: false,
+  loading: () => <p>Loading Component...</p>,
+});
+
+const AdminDocPage = () => {
+  return <DynamicSwaggerUI spec={swaggerFile} />;
 };
+
+export default AdminDocPage;
